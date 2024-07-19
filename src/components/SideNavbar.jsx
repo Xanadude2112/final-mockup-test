5f91a21b9d5c5169346ded0390641a4520a73a95
Tommy <tommy.azran@gmail.com>
2024-07-19 13:59:27 -0400

    set up bare bones

---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import "../styles/SideNavbar.scss"
+
+function SideNavbar() {
+  return( 
+    <div className="nav">
+      <h1 className="logo">TEAM8</h1>
+      <div></div>
+    </div>
+  )
+}
+
+export default SideNavbar;
